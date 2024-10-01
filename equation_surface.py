d801dc07cc0d8c3c5f8d871bd4859fe19c15ff62
Pascal Bauermeister <pascal.bauermeister@depsys.com>
2024-10-01 23:09:02 +0200

    Global refactoring, add typing/linting and vscode settings.

--- equation_surface.py
+++ equation_surface.py
@@ -1,34 +1,67 @@
 #!/usr/bin/python3
 
-import numpy
-from skimage import measure
-from matplotlib import pyplot
-from mpl_toolkits.mplot3d import Axes3D
+from typing import Callable, Any
+
+import numpy.typing as npt
 import stl
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D  # type: ignore[import-untyped]
+from numpy import mgrid  # type: ignore[attr-defined]
+from numpy import zeros
+from skimage import measure
+from dataclasses import dataclass
 
 ################################################################################
 
+NDARRAY = npt.NDArray[Any]
 
-def compute_volume(span, resolution, lattice_param, f):
-    x, y, z = span * numpy.mgrid[-1:1:resolution, -1:1:resolution, -1:1:resolution] * lattice_param
+Formula = Callable[[NDARRAY, NDARRAY, NDARRAY], NDARRAY]
+
+
+@dataclass
+class PlotParams:
+    name: str
+    lattice_param: float
+    resolution: complex
+    span: float
+    formula: Formula
+
+
+def compute_volume(
+    span: float, resolution: complex, lattice_param: float, f: Formula
+) -> tuple[NDARRAY, NDARRAY]:
+    mg = mgrid[-1:1:resolution, -1:1:resolution, -1:1:resolution]  # type: ignore
+    x, y, z = span * mg * lattice_param
     volume = f(x, y, z)
     vertices, faces, normals, vals = measure.marching_cubes(
-        volume, 0, spacing=(0.1, 0.1, 0.1), allow_degenerate=True
-    )
+        volume,
+        0,
+        spacing=(0.1, 0.1, 0.1),
+        allow_degenerate=True,
+    )  # type: ignore[no-untyped-call]
+
     return vertices, faces
 
 
-def save_stl(vertices, faces, filename):
-    data = numpy.zeros(faces.shape[0], dtype=stl.mesh.Mesh.dtype)
-    mesh = stl.mesh.Mesh(data, remove_empty_areas=False)
+def save_stl(vertices: NDARRAY, faces: NDARRAY, filename: str) -> None:
+    data = zeros(faces.shape[0], dtype=stl.mesh.Mesh.dtype)
+    mesh = stl.mesh.Mesh(data, remove_empty_areas=False)  # type: ignore[no-untyped-call]
     for i, f in enumerate(faces):
         for j in range(3):
             mesh.vectors[i][j] = vertices[f[j], :]
-    mesh.save(filename, mode=stl.Mode.ASCII)
+    mesh.save(filename, mode=stl.Mode.ASCII)  # type: ignore[no-untyped-call]
 
 
-def make_plot(vertices, faces):
+def make_plot(vertices: NDARRAY, faces: NDARRAY) -> None:
     fig = pyplot.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(vertices[:, 0], vertices[:, 1], faces, vertices[:, 2], cmap='ocean', lw=1)
-    return pyplot
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_trisurf(vertices[:, 0], vertices[:, 1], faces, vertices[:, 2], cmap="ocean", lw=1)  # type: ignore[attr-defined]
+
+
+def run(params: PlotParams) -> None:
+    vertices, faces = compute_volume(
+        params.span, params.resolution, params.lattice_param, params.formula
+    )
+    save_stl(vertices, faces, f"{params.name}.stl")
+    make_plot(vertices, faces)
+    pyplot.show()
--- gyroid.py
+++ gyroid.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 
-from equation_surface import compute_volume, save_stl, make_plot
-from numpy import sin, cos, pi, sqrt
+from numpy import cos, pi, sin
+from equation_surface import run, NDARRAY, PlotParams
 
-lattice_param = 1.0
-resolution = 31j
-span = pi
-strut_param = 0.0
+STRUT_PARAM = 0.0
 
 
-def gyroid(x, y, z):
-    return cos(x) * sin(y) + cos(y) * sin(z) + cos(z) * sin(x) - strut_param
+def gyroid(x: NDARRAY, y: NDARRAY, z: NDARRAY) -> NDARRAY:
+    return cos(x) * sin(y) + cos(y) * sin(z) + cos(z) * sin(x) - STRUT_PARAM  # type: ignore
 
 
-vertices, faces = compute_volume(span, resolution, lattice_param, gyroid)
-save_stl(vertices, faces, 'Gyroid.stl')
-plot = make_plot(vertices, faces)
+params = PlotParams(
+    name="gyroid",
+    lattice_param=1.0,
+    resolution=31j,
+    span=pi,
+    formula=gyroid,
+)
 
-plot.show()
+run(params)
